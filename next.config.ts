b019d66c1d945a61dded2fb6d03583bfbd76b65a
aodintsov1 <aodintsov4edu@gmail.com>
2025-04-28 13:18:16 -0700

    Enable static export, set basePath & assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  reactStrictMode: true,
+  basePath: process.env.NODE_ENV === "production" ? "/my-app" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/my-app/" : "",
   /* config options here */
 };
 
